448ddf9e03abe1a0fe1de1bd14fcdc9f51ad613a
alukart32 <alukart32@gmail.com>
2019-04-10 14:47:22 +0300

    Sync with WebStorm.

--- client/src/main.js
+++ client/src/main.js
@@ -1,8 +1,28 @@
 import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
+import VueRoute from 'vue-router'
 
+import BootstrapVue from 'bootstrap-vue'
+
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+import App from './App.vue'
+import Home from './components/Home'
+import Link1 from './components/Link1'
+
+Vue.use(BootstrapVue)
+Vue.use(VueRoute)
 Vue.config.productionTip = false
+
+const routes = [
+    {path: '/', component: Home},
+    {path: '/link1/:link1Id', component: Link1}
+]
+
+const router = new VueRoute({
+    routes,
+    mode: 'history'
+})
 
 new Vue({
   router,
